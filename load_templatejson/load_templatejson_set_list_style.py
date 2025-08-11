009fe15d27f4da2352b54df6baa632047b719d6b
sunknight314 <875319119@qq.com>
2025-08-11 21:58:53 +0800

    feat(template): 添加 JSON 配置模板功能
    - 新增 load_templatejson_set_list_style.py 文件，实现从 JSON 配置文件加载列表模板
    - 添加 manual_settings.html 文件，提供手动设置模板的 Web界面
    - 更新设置列表级别.py 文件，使用 constants 模块中的常量
    - 新增 word_template_config.json 文件，作为模板配置示例

---
+++ load_templatejson/load_templatejson_set_list_style.py
@@ -0,0 +1,107 @@
+import os
+import json
+import win32com.client as win32
+from constants.constants import *
+
+def load_template_from_json(json_path, docx_path):
+    # 读取JSON配置文件
+    with open(json_path, 'r', encoding='utf-8') as f:
+        config = json.load(f)
+    
+    # 启动Word应用程序
+    word = win32.Dispatch('Word.Application')
+    word.Visible = False  # 可以设置为True来查看Word操作过程
+    
+
+    # 打开文档
+    abs_doc_path = os.path.abspath(docx_path)
+    doc = word.Documents.Open(abs_doc_path)
+
+    # 获取样式集合
+    styles = doc.Styles
+
+    # 创建新的列表模板
+    newListTemplate = doc.ListTemplates.Add(Name="从JSON配置创建的模板")
+    newListTemplate.Convert(1)
+
+    # 遍历配置中的标题级别并设置格式
+    for i, heading in enumerate(config['headings']):
+        level_index = heading['OutlineLevel']  # Word中的级别从1开始
+
+        # 获取对应级别的列表级别对象
+        listLevel = newListTemplate.ListLevels(level_index)
+
+        # 设置列表级别属性
+        listLevel.NumberFormat = heading['NumberFormat']
+        listLevel.TrailingCharacter = heading['TrailingCharacter']
+        listLevel.NumberStyle = heading['NumberStyle']
+        listLevel.NumberPosition = heading['NumberPosition']
+        listLevel.Alignment = heading['Alignment']
+        listLevel.TextPosition = heading['TextPosition']
+        listLevel.TabPosition = heading['TabPosition']
+        listLevel.ResetOnHigher = heading['ResetOnHigher']
+        listLevel.StartAt = heading['StartAt']
+
+        # 创建对应样式
+        style_name = f"my10标题 {level_index}"
+
+        # 创建新样式
+        style = styles.Add(style_name, WdStyleType.wdStyleTypeParagraph)
+
+        # 设置字体属性
+        font = heading['Font']
+        style.Font.NameFarEast = font['NameFarEast']
+        style.Font.NameAscii = font['NameAscii']
+        style.Font.NameOther = font['NameOther']
+        style.Font.Bold = font['Bold']
+        style.Font.Size = font['Size']
+        # 注意：颜色需要转换十六进制到Word颜色值
+        if isinstance(font['Color'], str) and font['Color'].startswith('#'):
+            # 将十六进制颜色转换为RGB值
+            color_hex = font['Color'].lstrip('#')
+            color_value = int(color_hex, 16)
+            # Word使用BGR格式，需要转换
+            bgr_color = ((color_value & 0xFF) << 16) | (color_value & 0xFF00) | ((color_value & 0xFF0000) >> 16)
+            style.Font.Color = bgr_color
+        else:
+            style.Font.Color = 0x000000  # 默认黑色
+
+        # 设置段落格式
+        para_format = heading['ParagraphFormat']
+        style.ParagraphFormat.RightIndent = para_format['RightIndent']
+        style.ParagraphFormat.SpaceBefore = para_format['SpaceBefore']
+        style.ParagraphFormat.SpaceAfter = para_format['SpaceAfter']
+        style.ParagraphFormat.LineSpacing = para_format['LineSpacing']
+        style.ParagraphFormat.OutlineLevel = heading['OutlineLevel']
+
+        # 链接列表级别和样式
+        listLevel.LinkedStyle = style.NameLocal
+
+    # 应用列表模板到文档中的段落
+    for para in doc.Paragraphs:
+        ol_level = para.OutlineLevel
+        # 注意：Word中的OutlineLevel从1开始，而JSON配置中也是从1开始
+        if 1 <= ol_level <= len(config['headings']):
+            para.Range.ListFormat.ApplyListTemplateWithLevel(
+                newListTemplate
+            )
+
+    # 保存并关闭文档
+    # doc.Save()
+    # doc.Close()
+
+    print(f"成功应用配置到文档: {docx_path}")
+        
+
+
+if __name__ == "__main__":
+    # 使用示例
+    json_config_path = os.path.join(os.path.dirname(__file__), 'word_template_config.json')
+    docx_file_path = 'source_outlined.docx'  # 需要确保这个文件存在
+    
+    if os.path.exists(json_config_path) and os.path.exists(docx_file_path):
+        load_template_from_json(json_config_path, docx_file_path)
+    else:
+        print("错误：找不到配置文件或文档文件")
+        print(f"配置文件路径: {json_config_path}")
+        print(f"文档文件路径: {os.path.abspath(docx_file_path)}")
--- set_list_style/设置列表级别.py
+++ set_list_style/设置列表级别.py
@@ -1,6 +1,6 @@
 import os
 import win32com.client as win32
-from win32com.client import constants as c
+from constants.constants import *
 
 word = win32.Dispatch('Word.Application')
 
@@ -10,7 +10,7 @@ doc = word.Documents.Open(abs_doc_path)
 # 获取样式集合
 styles = doc.Styles
 
-newListTemplate = doc.ListTemplates.Add(Name="新列表模板1")
+newListTemplate = doc.ListTemplates.Add(Name="新列表模板2")
 newListTemplate.Convert(1)
 
 
@@ -18,48 +18,51 @@ newListTemplate.Convert(1)
 # 设置列表级别1的格式
 listLevel1 = newListTemplate.ListLevels(1)
 listLevel1.NumberFormat = "第%1章"
-listLevel1.TrailingCharacter = 1
-listLevel1.NumberStyle = 37
-listLevel1.NumberPosition = 0
-listLevel1.Alignment = 0
+listLevel1.TrailingCharacter = TrailingCharacter.wdTrailingSpace
+listLevel1.NumberStyle = NumberStyle.wdListNumberStyleSimpChinNum1
+listLevel1.NumberPosition = 32
+listLevel1.Alignment = WdListLevelAlignment.wdListLevelAlignLeft
 listLevel1.TextPosition = 0
 listLevel1.TabPosition = 9999999
 listLevel1.ResetOnHigher = 0
 listLevel1.StartAt = 1
 
-style1 = styles.Add("my6标题 1", 1)
+style1 = styles.Add("my7标题 1", 1)
 style1.Font.NameFarEast = "黑体"
 style1.Font.NameAscii = "Times New Roman"
 style1.Font.NameOther = "Times New Roman"
 style1.Font.Bold = False
 style1.Font.Size = 16
+style1.Font.Color = 0x000000  # 黑色
 
 style1.ParagraphFormat.RightIndent = 0
 style1.ParagraphFormat.SpaceBefore = 24
 style1.ParagraphFormat.SpaceAfter = 18
 style1.ParagraphFormat.LineSpacing = 20
 style1.ParagraphFormat.OutlineLevel = 1
+
 listLevel1.LinkedStyle = style1
 
 # ---------------------------------------------------------------------
 # 设置列表级别2的格式
 listLevel2 = newListTemplate.ListLevels(2)
 listLevel2.NumberFormat = "%1.%2"
-listLevel2.TrailingCharacter = 1
-listLevel2.NumberStyle = 253
+listLevel2.TrailingCharacter = TrailingCharacter.wdTrailingSpace
+listLevel2.NumberStyle = NumberStyle.wdListNumberStyleLegal
 listLevel2.NumberPosition = 0
-listLevel2.Alignment = 0
+listLevel2.Alignment = WdListLevelAlignment.wdListLevelAlignLeft
 listLevel2.TextPosition = 0
 listLevel2.TabPosition = 9999999
 listLevel2.ResetOnHigher = 1
 listLevel2.StartAt = 1
 
-style2 = styles.Add("my6标题 2", 1)
+style2 = styles.Add("my7标题 2", 1)
 style2.Font.NameFarEast = "宋体"
 style2.Font.NameAscii = "Times New Roman"
 style2.Font.NameOther = "Times New Roman"
 style2.Font.Bold = True
 style2.Font.Size = 15
+style1.Font.Color = 0x000000  # 黑色
 
 style2.ParagraphFormat.RightIndent = 0
 style2.ParagraphFormat.SpaceBefore = 18
@@ -74,16 +77,16 @@ listLevel2.LinkedStyle = style2
 # 设置列表级别3的格式
 listLevel3 = newListTemplate.ListLevels(3)
 listLevel3.NumberFormat = "%1.%2.%3"
-listLevel3.TrailingCharacter = 1
-listLevel3.NumberStyle = 253
+listLevel3.TrailingCharacter = TrailingCharacter.wdTrailingSpace
+listLevel3.NumberStyle = NumberStyle.wdListNumberStyleLegal
 listLevel3.NumberPosition = 28
-listLevel3.Alignment = 0
+listLevel3.Alignment = WdListLevelAlignment.wdListLevelAlignLeft
 listLevel3.TextPosition = 0
 listLevel3.TabPosition = 9999999
 listLevel3.ResetOnHigher = 2
 listLevel3.StartAt = 1
 
-style3 = styles.Add("my6标题 3", 1)
+style3 = styles.Add("my7标题 3", 1)
 style3.Font.NameFarEast = "宋体"
 style3.Font.NameAscii = "Times New Roman"
 style3.Font.NameOther = "Times New Roman"
